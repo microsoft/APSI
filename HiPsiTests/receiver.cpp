7bdd84c5d6bb7ca1be322c220dd695e82cb44696
Kim Laine <kim.laine@microsoft.com>
2017-04-30 00:48:04 -0700

    Added test files

---
+++ HiPsiTests/receiver.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace HiPsiTests
+{
+	TEST_CLASS(ReceiverTests)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+	};
+}
--- HiPsiTests/sender.cpp
+++ HiPsiTests/sender.cpp
@@ -5,7 +5,7 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace HiPsiTests
 {		
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(SenderTests)
 	{
 	public:
 		
