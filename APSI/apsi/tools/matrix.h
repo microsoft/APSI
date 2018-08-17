0eabadd277ece3b0b1b5e0c88c4c34fa675f0fb3
Radames Cruz Moreno <v-rcruz@microsoft.com>
2018-08-16 17:56:09 -0700

    Add missing comments and remove unnecessary field.

--- APSI/apsi/tools/matrix.h
+++ APSI/apsi/tools/matrix.h
@@ -11,7 +11,7 @@ using namespace std;
  * Simple bi-dimensional Matrix.
  * 
  * - Deletes its data when destroyed.
- * - Allows resizing
+ * - Allows resizing.
  */
 template<class T>
 class Matrix : public MatrixView<T>
@@ -19,32 +19,34 @@ class Matrix : public MatrixView<T>
 public:
     Matrix() = default;
 
-    Matrix(u64 rows, u64 cols) :
-        capacity_(0)
+    Matrix(u64 rows, u64 cols)
     {
         resize(rows, cols);
     }
 
+    /**
+     * Resize the matrix.
+     * If the needed capacity exceeds the current capacity, will allocate more
+     * memory and preserve existing data.
+     * When the needed capacity is less than the current capacity, the memory
+     * use will remain the same and the matrix will be reduced only logically.
+     */
     void resize(u64 newRows, u64 newCols)
     {
         u64 newCapacity = newRows * newCols;
 
-        if (newCapacity > capacity_)
+        if (newCapacity > data_.size())
         {
             data_.resize(newCapacity);
-            capacity_ = newCapacity;
         }
 
         if (newRows != this->rows() || newCols != this->columns())
         {
             MatrixView<T>::resize(data_.data(), newRows, newCols);
         }
-
-        capacity_ = newCapacity;
     }
 
 private:
-    u64 capacity_;
     vector<T> data_;
 };
 
