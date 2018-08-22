0adad20d5498ae5c1f9bfca324f4356cc4ac0aa7
Radames S. Cruz Moreno <v-rcruz@microsoft.com>
2018-08-22 12:10:36 -0700

    Add comment

--- APSI/apsi/tools/prng.h
+++ APSI/apsi/tools/prng.h
@@ -8,6 +8,12 @@ namespace apsi
 {
     namespace tools
     {
+        /**
+         * Deterministic Pseudo Random Number Generator
+         *
+         * This is a wrapper over crypto++'s Hash_DRBG class, to allow
+         * using our types as seeds.
+         */
         class DPRNG : public CryptoPP::Hash_DRBG</* HASH */ CryptoPP::SHA256, /* STRENGTH */ 16U, /* SEEDLENGTH */ 55U>
         {
         public:
