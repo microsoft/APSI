f8fb9e2c0c9a4eae361db883e75ffeffb9584fe6
Radames S. Cruz Moreno <v-rcruz@microsoft.com>
2018-09-07 17:09:06 -0700

    psiparams.cpp is now unnecessary

--- APSI/apsi/psiparams.h
+++ APSI/apsi/psiparams.h
@@ -75,7 +75,29 @@ namespace apsi
         {
         }
 
-        void validate() const;
+        void validate() const
+        {
+            if (sender_bin_size_ % split_count_ != 0)
+            {
+                throw std::invalid_argument("Sender bin size must be a multiple of number of splits.");
+            }
+
+            if ((item_bit_count_ + 63) / 64 != (item_bit_count_ + static_cast<int>(floor(log2(hash_func_count_))) + 1 + 1 + 63) / 64)
+            {
+                throw std::invalid_argument("Invalid for cuckoo: null bit and location index overflow to new uint64_t.");
+            }
+
+            if (item_bit_count_ > max_item_bit_count)
+            {
+                throw std::invalid_argument("Item bit count cannot exceed max.");
+            }
+
+            if (item_bit_count_ > (max_item_bit_count - 8))
+            {
+                // Not an error, but a warning.
+                std::cout << std::endl << "Item bit count is close to its upper limit. Several bits should be reserved for appropriate Cuckoo hashing." << std::endl;
+            }
+        }
 
         inline bool use_oprf() const
         {
