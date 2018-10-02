e2d867db4a76e4ae3632e1320fdc81b73413c024
Radames S. Cruz Moreno <v-rcruz@microsoft.com>
2018-10-02 15:29:58 -0700

    Rename Sender/Receiver
    Rename SenderExample -> Sender, ReceiverExample -> Receiver.

---
+++ CommonCode/common_utils.h
@@ -0,0 +1,48 @@
+#pragma once
+
+// STD
+#include <string>
+#include <vector>
+
+// APSI
+#include "apsi/tools/stopwatch.h"
+
+
+namespace apsi
+{
+    class BaseCLP;
+    class PSIParams;
+
+    namespace tools
+    {
+        /**
+        Print a banner with asterisks on top and bottom
+        */
+        void print_example_banner(const std::string title);
+
+        /**
+        Prepare console for color output.
+        */
+        void prepare_console();
+
+        /**
+        Get a PSIParams object from a command line.
+        */
+        const PSIParams build_psi_params(
+            const BaseCLP& cmd,
+            const apsi::u64 sender_set_size = 0,
+            const int item_bit_length = 0,
+            const int label_bit_length = 0,
+            const bool use_oprf = false);
+
+        /**
+        Generate timing report for timespans
+        */
+        void generate_timespan_report(std::vector<std::string>& report, const std::vector<apsi::tools::Stopwatch::TimespanSummary>& timespans, int max_name_length);
+
+        /**
+        Generate timing report for single events
+        */
+        void generate_event_report(std::vector<std::string>& report, const std::vector<apsi::tools::Stopwatch::Timepoint>& timepoints, int max_name_length);
+    }
+}
---
+++ Sender/clp.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "base_clp.h"
+
+namespace apsi
+{
+    /**
+    Command Line Processor for SenderExample
+    */
+    class CLP : public BaseCLP
+    {
+    public:
+        CLP(const std::string& desc)
+            : BaseCLP(desc)
+        {}
+
+        virtual void add_args()
+        {
+            add(labels_arg_);
+            add(oprf_arg_);
+            add(item_bit_length_arg_);
+            add(db_file_arg_);
+            add(net_port_arg_);
+        }
+
+        virtual void get_args()
+        {
+            use_labels_ = labels_arg_.getValue();
+            cout_param("useLabels", use_labels_ ? "true" : "false");
+
+            oprf_ = oprf_arg_.getValue();
+            cout_param("oprf", oprf_ ? "true" : "false");
+
+            item_bit_length_ = item_bit_length_arg_.getValue();
+            cout_param("itemBitLength", item_bit_length_);
+
+            db_file_ = db_file_arg_.getValue();
+            cout_param("db", db_file_);
+
+            net_port_ = net_port_arg_.getValue();
+            cout_param("port", net_port_);
+        }
+
+        bool use_labels() const { return use_labels_; }
+        bool use_oprf() const { return oprf_; }
+        unsigned item_bit_length() const { return item_bit_length_; }
+        int net_port() const { return net_port_; }
+        const std::string& db_file() const { return db_file_; }
+
+    private:
+        TCLAP::SwitchArg labels_arg_ = TCLAP::SwitchArg("l", "useLabels", "Use labels", false);
+        TCLAP::SwitchArg oprf_arg_ = TCLAP::SwitchArg("o", "oprf", "Use OPRF", false);
+        TCLAP::ValueArg<unsigned> item_bit_length_arg_ = TCLAP::ValueArg<unsigned>("b", "itemBitLength", "Item bit length", false, 60, "unsigned");
+        TCLAP::ValueArg<int> net_port_arg_ = TCLAP::ValueArg<int>("", "port", "Network port to bind to", false, 1212, "int");
+        TCLAP::ValueArg<std::string> db_file_arg_ = TCLAP::ValueArg<std::string>("", "db", "Path to the file containing the Sender database", true, "", "string");
+
+        bool use_labels_;
+        bool oprf_;
+        unsigned item_bit_length_;
+        int net_port_;
+        std::string db_file_;
+    };
+}
