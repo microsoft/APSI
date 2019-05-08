8e7b8873994a15809d760f9a0c244b8ae4c528cc
Radames S. Cruz Moreno <v-rcruz@microsoft.com>
2019-05-08 10:16:49 -0700

    Remove 'DLL' from symbol names.

--- APSINative/apsinative.h
+++ APSINative/apsinative.h
@@ -10,7 +10,7 @@
 //
 ///////////////////////////////////////////////////////////////////////////
 
-#define APSIDLL extern "C" __declspec(dllexport)
+#define APSIEXPORT extern "C" __declspec(dllexport)
 #define APSICALL __cdecl
 
 typedef unsigned long long u64;
@@ -21,17 +21,17 @@ Connect a Receiver to the given address and port.
 Connecting will internally create a Receiver instance
 and initialize it by performing a handshake with the Sender.
 */
-APSIDLL bool APSICALL ReceiverConnect(char* address, int port);
+APSIEXPORT bool APSICALL ReceiverConnect(char* address, int port);
 
 /**
 Disconnect a Receiver.
 */
-APSIDLL void APSICALL ReceiverDisconnect();
+APSIEXPORT void APSICALL ReceiverDisconnect();
 
 /**
 Returns whether the Receiver is connected
 */
-APSIDLL bool APSICALL ReceiverIsConnected();
+APSIEXPORT bool APSICALL ReceiverIsConnected();
 
 /**
 Perform a Query for the given items.
@@ -39,4 +39,4 @@ Perform a Query for the given items.
 The 'result' array consist of booleans encoded as integers. Any value other than 0
 is considered 'true', 0 is considered as 'false'.
 */
-APSIDLL bool APSICALL ReceiverQuery(int length, u64* items, int* result, u64* labels);
+APSIEXPORT bool APSICALL ReceiverQuery(int length, u64* items, int* result, u64* labels);
