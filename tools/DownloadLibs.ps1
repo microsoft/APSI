2d3824e48be51a612e4aaa301976e2a16e042607
Radames S. Cruz Moreno <v-rcruz@microsoft.com>
2019-07-19 16:06:04 -0700

    Update download URL for libs

--- tools/DownloadLibs.ps1
+++ tools/DownloadLibs.ps1
@@ -8,6 +8,6 @@ $LibsZipFile = Join-Path -Path $LibsDirectory -ChildPath Libs.zip
 
 mkdir -Path $LibsDirectory
 
-Invoke-WebRequest -Uri "https://vmcreator.file.core.windows.net/apsilibs/Libs.zip?sp=rl&st=2019-07-19T21:12:33Z&se=2021-07-20T21:12:00Z&sv=2018-03-28&sig=aMOA0ff%2F%2BL6fm3ZytXZl1q%2FKbKoedsYD6A9v6xeognk%3D&sr=f" -OutFile $LibsZipFile
+Invoke-WebRequest -Uri "https://vmcreator.file.core.windows.net/apsilibs/Libs.zip?sp=rl&st=2019-07-19T22:49:29Z&se=2021-07-20T22:49:00Z&sv=2018-03-28&sig=osN%2FBq8YOWOCwRAhW3bhnWqqW59CvdsOKztVlmN09E0%3D&sr=f" -OutFile $LibsZipFile
 Expand-Archive -Path $LibsZipFile -DestinationPath $LibsDirectory
 
