20a12a6039014285509c8e0b0fb0100b8b42afc9
Kim Laine <kim.laine@microsoft.com>
2020-08-09 17:51:47 -0700

    StreamChannel tests done

--- tests/unit/src/stream_channel.cpp
+++ tests/unit/src/stream_channel.cpp
@@ -1,28 +1,71 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-#include "seal/encryptionparams.h"
+// STD
+#include <string>
+#include <thread>
+#include <utility>
+
+// SEAL
 #include "seal/keygenerator.h"
-#include "seal/relinkeys.h"
+#include "seal/publickey.h"
+
+// APSI
 #include "apsi/network/stream_channel.h"
+#include "apsi/util/utils.h"
+
 #include "gtest/gtest.h"
 
 using namespace std;
+using namespace seal;
 using namespace apsi;
 using namespace apsi::network;
-using namespace seal;
-
-namespace
-{
-    template <typename... Ts>
-    vector<SEAL_BYTE> CreateByteVector(Ts &&... args)
-    {
-        return { SEAL_BYTE(forward<Ts>(args))... };
-    }
-} // namespace
 
 namespace APSITests
 {
+    namespace
+    {
+        shared_ptr<PSIParams> get_params()
+        {
+            static shared_ptr<PSIParams> params = nullptr;
+            if (!params)
+            {
+                PSIParams::ItemParams item_params;
+                item_params.felts_per_item = 8;
+
+                PSIParams::TableParams table_params;
+                table_params.hash_func_count = 3;
+                table_params.max_items_per_bin = 16;
+                table_params.table_size = 512;
+                table_params.window_size = 1;
+
+                size_t pmd = 4096;
+                PSIParams::SEALParams seal_params(scheme_type::BFV);
+                seal_params.set_poly_modulus_degree(pmd);
+                seal_params.set_coeff_modulus(CoeffModulus::BFVDefault(pmd));
+                seal_params.set_plain_modulus(65537);
+
+                params = make_shared<PSIParams>(item_params, table_params, seal_params);
+            }
+
+            return params;
+        }
+
+        shared_ptr<CryptoContext> get_context()
+        {
+            static shared_ptr<CryptoContext> context = nullptr;
+            if (!context)
+            {
+                context = make_shared<CryptoContext>(SEALContext::Create(get_params()->seal_params()));
+                KeyGenerator keygen(context->seal_context());
+                context->set_secret(keygen.secret_key());
+                context->set_evaluator(keygen.relin_keys_local());
+            }
+
+            return context;
+        }
+    } // namespace
+
     class StreamChannelTests : public ::testing::Test
     {
     protected:
@@ -33,261 +76,166 @@ namespace APSITests
         {}
     };
 
-    TEST_F(StreamChannelTests, SendGetParametersTest)
+    TEST_F(StreamChannelTests, SendReceiveParmsTest)
     {
         stringstream stream1;
         stringstream stream2;
 
-        StreamChannel senderchannel(/* istream */ stream1, /* ostream */ stream2);
-        StreamChannel receiverchannel(/* istream */ stream2, /* ostream */ stream1);
+        StreamChannel svr(/* istream */ stream1, /* ostream */ stream2);
+        StreamChannel clt(/* istream */ stream2, /* ostream */ stream1);
 
-        receiverchannel.send_get_parameters();
-        stream1.seekp(0);
+        unique_ptr<SenderOperation> sop = make_unique<SenderOperationParms>();
 
-        shared_ptr<SenderOperation> sender_op;
-        senderchannel.receive(sender_op);
+        // Send a parms operation
+        clt.send(move(sop));
 
-        ASSERT_EQ(SOP_get_parameters, sender_op->type);
+        sop = svr.receive_operation(get_context()->seal_context());
+        ASSERT_NE(nullptr, sop);
+        ASSERT_EQ(SenderOperationType::SOP_PARMS, sop->type());
+
+        // Create a parms response
+        auto rsop_parms = make_unique<SenderOperationResponseParms>();
+        rsop_parms->params = make_unique<PSIParams>(*get_params());
+        unique_ptr<SenderOperationResponse> rsop = move(rsop_parms);
+        svr.send(move(rsop));
+
+        // Receive the parms response
+        rsop = clt.receive_response(SenderOperationType::SOP_PARMS);
+        rsop_parms.reset(dynamic_cast<SenderOperationResponseParms*>(rsop.release()));
+
+        // We received valid parameters
+        ASSERT_EQ(get_params()->item_bit_count(), rsop_parms->params->item_bit_count());
+
+        ASSERT_EQ(svr.bytes_sent(), clt.bytes_received());
+        ASSERT_EQ(svr.bytes_received(), clt.bytes_sent());
     }
 
-    TEST_F(StreamChannelTests, SendGetParameterResponseTest)
+    TEST_F(StreamChannelTests, SendReceiveOPRFTest)
     {
         stringstream stream1;
         stringstream stream2;
 
-        StreamChannel senderchannel(/* istream */ stream1, /* ostream */ stream2);
-        StreamChannel receiverchannel(/* istream */ stream2, /* ostream */ stream1);
+        StreamChannel svr(/* istream */ stream1, /* ostream */ stream2);
+        StreamChannel clt(/* istream */ stream2, /* ostream */ stream1);
 
-        PSIParams::PSIConfParams psiconf_params{ 12345, 128, 60, 90, 45, false, false };
-        PSIParams::TableParams table_params{ 10, 1, 2, 35, 40, true };
-        PSIParams::CuckooParams cuckoo_params{ 3, 2, 1 };
-        PSIParams::FFieldParams ffield_params{ 678910, 8 };
-        PSIParams::SEALParams seal_params;
-        seal_params.max_supported_degree = 20;
-        seal_params.encryption_params.set_plain_modulus(5119);
-        seal_params.encryption_params.set_poly_modulus_degree(4096);
-        vector<Modulus> coeff_modulus = CoeffModulus::BFVDefault(seal_params.encryption_params.poly_modulus_degree());
-        seal_params.encryption_params.set_coeff_modulus(coeff_modulus);
+        // Fill a data buffer
+        vector<SEAL_BYTE> oprf_data(256);
+        for (size_t i = 0; i < oprf_data.size(); i++)
+        {
+            oprf_data[i] = static_cast<SEAL_BYTE>(i);
+        }
 
-        PSIParams params(psiconf_params, table_params, cuckoo_params, seal_params, ffield_params);
+        auto sop_oprf = make_unique<SenderOperationOPRF>();
+        sop_oprf->data = oprf_data;
+        unique_ptr<SenderOperation> sop = move(sop_oprf);
 
-        vector<SEAL_BYTE> client_id;
-        senderchannel.send_get_parameters_response(client_id, params);
-        stream2.seekp(0);
+        // Send an OPRF operation
+        clt.send(move(sop));
 
-        SenderResponseGetParameters gpr;
-        receiverchannel.receive(gpr);
+        // Receive the operation
+        sop = svr.receive_operation(get_context()->seal_context());
+        ASSERT_EQ(SenderOperationType::SOP_OPRF, sop->type());
+        sop_oprf.reset(dynamic_cast<SenderOperationOPRF*>(sop.release()));
 
-        ASSERT_EQ(60, gpr.psiconf_params.item_bit_count);
-        ASSERT_EQ(false, gpr.psiconf_params.use_labels);
-        ASSERT_EQ(false, gpr.psiconf_params.use_fast_membership);
-        ASSERT_EQ((uint64_t)12345, gpr.psiconf_params.sender_size);
-        ASSERT_EQ(size_t(90), gpr.psiconf_params.item_bit_length_used_after_oprf);
-        ASSERT_EQ(45, gpr.psiconf_params.num_chunks);
-        ASSERT_EQ(128, gpr.psiconf_params.sender_bin_size);
+        // Validate the data
+        ASSERT_EQ(256, sop_oprf->data.size());
+        for (size_t i = 0; i < sop_oprf->data.size(); i++)
+        {
+            ASSERT_EQ(static_cast<char>(sop_oprf->data[i]), static_cast<char>(i));
+        }
 
-        ASSERT_EQ(10, gpr.table_params.log_table_size);
-        ASSERT_EQ(1, gpr.table_params.window_size);
-        ASSERT_EQ(2, gpr.table_params.split_count);
-        ASSERT_EQ(35, gpr.table_params.split_size);
-        ASSERT_EQ(40, gpr.table_params.binning_sec_level);
-        ASSERT_TRUE(gpr.table_params.use_dynamic_split_count);
+        // Create an OPRF response
+        auto rsop_oprf = make_unique<SenderOperationResponseOPRF>();
+        rsop_oprf->data = oprf_data;
+        unique_ptr<SenderOperationResponse> rsop = move(rsop_oprf);
+        svr.send(move(rsop));
 
-        ASSERT_EQ(3, gpr.cuckoo_params.hash_func_count);
-        ASSERT_EQ(2, gpr.cuckoo_params.hash_func_seed);
-        ASSERT_EQ(1, gpr.cuckoo_params.max_probe);
+        // Receive the OPRF response
+        rsop = clt.receive_response(SenderOperationType::SOP_OPRF);
+        rsop_oprf.reset(dynamic_cast<SenderOperationResponseOPRF*>(rsop.release()));
 
-        ASSERT_EQ((uint64_t)678910, gpr.ffield_params.characteristic);
-        ASSERT_EQ(8, gpr.ffield_params.degree);
+        // Validate the data
+        ASSERT_EQ(256, rsop_oprf->data.size());
+        for (size_t i = 0; i < rsop_oprf->data.size(); i++)
+        {
+            ASSERT_EQ(static_cast<char>(rsop_oprf->data[i]), static_cast<char>(i));
+        }
 
-        ASSERT_EQ(20, gpr.seal_params.max_supported_degree);
-        ASSERT_EQ((uint64_t)5119, gpr.seal_params.encryption_params.plain_modulus().value());
-        ASSERT_EQ((size_t)4096, gpr.seal_params.encryption_params.poly_modulus_degree());
-        ASSERT_EQ((size_t)3, gpr.seal_params.encryption_params.coeff_modulus().size());
+        ASSERT_EQ(svr.bytes_sent(), clt.bytes_received());
+        ASSERT_EQ(svr.bytes_received(), clt.bytes_sent());
     }
 
-    TEST_F(StreamChannelTests, SendPreprocessTest)
+    TEST_F(StreamChannelTests, SendReceiveQueryTest)
     {
         stringstream stream1;
         stringstream stream2;
 
-        StreamChannel senderchannel(/* istream */ stream1, /* ostream */ stream2);
-        StreamChannel receiverchannel(/* istream */ stream2, /* ostream */ stream1);
+        StreamChannel svr(/* istream */ stream1, /* ostream */ stream2);
+        StreamChannel clt(/* istream */ stream2, /* ostream */ stream1);
 
-        vector<SEAL_BYTE> items = CreateByteVector(10, 20, 30, 40, 50);
+        auto sop_query = make_unique<SenderOperationQuery>();
+        sop_query->relin_keys = *get_context()->relin_keys();
+        sop_query->data[0].push_back(get_context()->encryptor()->encrypt_zero_symmetric());
+        sop_query->data[123].push_back(get_context()->encryptor()->encrypt_zero_symmetric());
+        unique_ptr<SenderOperation> sop = move(sop_query);
 
-        receiverchannel.send_preprocess(items);
-        stream1.seekp(0);
+        // Send a query operation
+        clt.send(move(sop));
 
-        shared_ptr<SenderOperation> sender_op;
-        senderchannel.receive(sender_op);
+        // Receive the operation
+        sop = svr.receive_operation(get_context()->seal_context());
+        ASSERT_EQ(SenderOperationType::SOP_QUERY, sop->type());
+        sop_query.reset(dynamic_cast<SenderOperationQuery*>(sop.release()));
 
-        ASSERT_EQ(SOP_preprocess, sender_op->type);
+        // Are we able to extract the relinearization keys?
+        ASSERT_NO_THROW(auto rlk = sop_query->relin_keys.extract_local());
 
-        shared_ptr<SenderOperationPreprocess> preprocess_op =
-            dynamic_pointer_cast<SenderOperationPreprocess>(sender_op);
-        ASSERT_TRUE(nullptr != preprocess_op);
+        // Check for query ciphertexts
+        ASSERT_EQ(2, sop_query->data.size());
 
-        ASSERT_EQ((size_t)5, preprocess_op->buffer.size());
-        ASSERT_EQ((unsigned char)10, (unsigned char)preprocess_op->buffer[0]);
-        ASSERT_EQ((unsigned char)20, (unsigned char)preprocess_op->buffer[1]);
-        ASSERT_EQ((unsigned char)30, (unsigned char)preprocess_op->buffer[2]);
-        ASSERT_EQ((unsigned char)40, (unsigned char)preprocess_op->buffer[3]);
-        ASSERT_EQ((unsigned char)50, (unsigned char)preprocess_op->buffer[4]);
-    }
+        ASSERT_FALSE(sop_query->data.at(0).empty());
+        ASSERT_EQ(1, sop_query->data[0].size());
+        auto query_ct0 = sop_query->data[0][0].extract_local();
 
-    TEST_F(StreamChannelTests, SendPreprocessResponseTest)
-    {
-        stringstream stream1;
-        stringstream stream2;
+        ASSERT_FALSE(sop_query->data.at(123).empty());
+        ASSERT_EQ(1, sop_query->data[123].size());
+        auto query_ct123 = sop_query->data[123][0].extract_local();
 
-        StreamChannel senderchannel(/* istream */ stream1, /* ostream */ stream2);
-        StreamChannel receiverchannel(/* istream */ stream2, /* ostream */ stream1);
+        // Create a query response
+        auto rsop_query = make_unique<SenderOperationResponseQuery>();
+        rsop_query->package_count = 2;
+        unique_ptr<SenderOperationResponse> rsop = move(rsop_query);
+        svr.send(move(rsop));
 
-        vector<SEAL_BYTE> buffer = CreateByteVector(100, 95, 80, 75, 60, 55, 40, 35, 20, 15, 10, 4, 3, 2, 1);
+        // Receive the query response
+        rsop = clt.receive_response(SenderOperationType::SOP_QUERY);
+        rsop_query.reset(dynamic_cast<SenderOperationResponseQuery*>(rsop.release()));
 
-        vector<SEAL_BYTE> client_id;
-        senderchannel.send_preprocess_response(client_id, buffer);
-        stream2.seekp(0);
+        // Validate the data
+        ASSERT_EQ(2, rsop_query->package_count);
 
-        SenderResponsePreprocess pr;
-        receiverchannel.receive(pr);
+        // Send two ResultPackages
+        auto rp = make_unique<ResultPackage>();
+        rp->bundle_idx = 0;
+        rp->psi_result = query_ct0;
+        svr.send(move(rp));
 
-        ASSERT_EQ((size_t)15, pr.buffer.size());
-        ASSERT_EQ((unsigned char)100, (unsigned char)pr.buffer[0]);
-        ASSERT_EQ((unsigned char)95, (unsigned char)pr.buffer[1]);
-        ASSERT_EQ((unsigned char)80, (unsigned char)pr.buffer[2]);
-        ASSERT_EQ((unsigned char)75, (unsigned char)pr.buffer[3]);
-        ASSERT_EQ((unsigned char)60, (unsigned char)pr.buffer[4]);
-        ASSERT_EQ((unsigned char)55, (unsigned char)pr.buffer[5]);
-        ASSERT_EQ((unsigned char)40, (unsigned char)pr.buffer[6]);
-        ASSERT_EQ((unsigned char)35, (unsigned char)pr.buffer[7]);
-        ASSERT_EQ((unsigned char)20, (unsigned char)pr.buffer[8]);
-        ASSERT_EQ((unsigned char)15, (unsigned char)pr.buffer[9]);
-        ASSERT_EQ((unsigned char)10, (unsigned char)pr.buffer[10]);
-        ASSERT_EQ((unsigned char)4, (unsigned char)pr.buffer[11]);
-        ASSERT_EQ((unsigned char)3, (unsigned char)pr.buffer[12]);
-        ASSERT_EQ((unsigned char)2, (unsigned char)pr.buffer[13]);
-        ASSERT_EQ((unsigned char)1, (unsigned char)pr.buffer[14]);
-    }
+        rp = make_unique<ResultPackage>();
+        rp->bundle_idx = 123;
+        rp->psi_result = query_ct123;
+        rp->label_result.push_back(query_ct123);
+        svr.send(move(rp));
 
-    TEST_F(StreamChannelTests, SendQueryTest)
-    {
-        stringstream stream1;
-        stringstream stream2;
-        StreamChannel senderchannel(/* istream */ stream1, /* ostream */ stream2);
-        StreamChannel receiverchannel(/* istream */ stream2, /* ostream */ stream1);
+        // Receive two packages
+        rp = clt.receive_result_package(get_context()->seal_context());
+        ASSERT_EQ(0, rp->bundle_idx);
+        ASSERT_TRUE(rp->label_result.empty());
 
-        EncryptionParameters enc_params(scheme_type::BFV);
-        enc_params.set_plain_modulus(64ul);
-        enc_params.set_poly_modulus_degree(4096);
-        enc_params.set_coeff_modulus(CoeffModulus::BFVDefault(4096));
-        shared_ptr<SEALContext> context = SEALContext::Create(enc_params);
-        KeyGenerator key_gen(context);
+        rp = clt.receive_result_package(get_context()->seal_context());
+        ASSERT_EQ(123, rp->bundle_idx);
+        ASSERT_EQ(1, rp->label_result.size());
 
-        stringstream ss;
-        Serializable<RelinKeys> ser_relinkeys = key_gen.relin_keys();
-        ser_relinkeys.save(ss, compr_mode_type::none);
-        string relin_keys_str = ss.str();
-        ASSERT_EQ(relin_keys_str.length(), 197010);
-
-        Ciphertext ct(context);
-        ss = stringstream();
-        ct.save(ss, compr_mode_type::none);
-        string ct_str = ss.str();
-        ASSERT_EQ(ct_str.length(), 105);
-
-        map<uint64_t, vector<string>> query;
-
-        vector<string> vec;
-        vec.push_back(ct_str);
-        vec.push_back(ct_str);
-        query.insert_or_assign(5, vec);
-
-        vec.clear();
-        vec.push_back(ct_str);
-        query.insert_or_assign(10, vec);
-
-        receiverchannel.send_query(relin_keys_str, query);
-        stream1.seekp(0);
-
-        shared_ptr<SenderOperation> sender_op;
-        senderchannel.receive(sender_op);
-
-        ASSERT_EQ(SOP_query, sender_op->type);
-
-        shared_ptr<SenderOperationQuery> query_op = dynamic_pointer_cast<SenderOperationQuery>(sender_op);
-        ASSERT_TRUE(nullptr != query_op);
-
-        ASSERT_TRUE(query_op->relin_keys.length() > 0);
-        ASSERT_EQ((size_t)2, query_op->query.size());
-        ASSERT_EQ((size_t)2, query_op->query.at(5).size());
-        ASSERT_EQ((size_t)1, query_op->query.at(10).size());
-    }
-
-    TEST_F(StreamChannelTests, SendQueryResponseTest)
-    {
-        stringstream stream1;
-        stringstream stream2;
-        StreamChannel senderchannel(/* istream */ stream1, /* ostream */ stream2);
-        StreamChannel receiverchannel(/* istream */ stream2, /* ostream */ stream1);
-
-        vector<SEAL_BYTE> client_id;
-        senderchannel.send_query_response(client_id, 50);
-        stream2.seekp(0);
-
-        SenderResponseQuery rq;
-        receiverchannel.receive(rq);
-
-        ASSERT_EQ((uint64_t)50, rq.package_count);
-    }
-
-    TEST_F(StreamChannelTests, SendPackageTest)
-    {
-        stringstream stream1;
-        stringstream stream2;
-        StreamChannel senderchannel(/* istream */ stream1, /* ostream */ stream2);
-        StreamChannel receiverchannel(/* istream */ stream2, /* ostream */ stream1);
-
-        ResultPackage pkg;
-
-        pkg.bundle_idx = 1;
-        pkg.data = "One";
-        pkg.label_data = "Two";
-
-        vector<SEAL_BYTE> client_id;
-        senderchannel.send(client_id, pkg);
-
-        pkg.bundle_idx = 3;
-        pkg.data = "Three";
-        pkg.label_data = "Four";
-
-        senderchannel.send(client_id, pkg);
-
-        pkg.bundle_idx = 5;
-        pkg.data = "Five";
-        pkg.label_data = "Six";
-
-        senderchannel.send(client_id, pkg);
-        stream2.seekp(0);
-
-        ResultPackage received;
-        receiverchannel.receive(received);
-
-        ASSERT_EQ(1, received.bundle_idx);
-        ASSERT_TRUE(received.data == "One");
-        ASSERT_TRUE(received.label_data == "Two");
-
-        receiverchannel.receive(received);
-
-        ASSERT_EQ(3, received.bundle_idx);
-        ASSERT_TRUE(received.data == "Three");
-        ASSERT_TRUE(received.label_data == "Four");
-
-        receiverchannel.receive(received);
-
-        ASSERT_EQ(5, received.bundle_idx);
-        ASSERT_TRUE(received.data == "Five");
-        ASSERT_TRUE(received.label_data == "Six");
+        ASSERT_EQ(svr.bytes_sent(), clt.bytes_received());
+        ASSERT_EQ(svr.bytes_received(), clt.bytes_sent());
     }
 } // namespace APSITests
