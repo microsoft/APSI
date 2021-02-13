9bb2044382fa8d1b89665b665ae6394907903652
Radames S. Cruz Moreno <racruzmo@microsoft.com>
2021-02-13 13:24:43 -0800

    Refactor cuckoo table index computation

--- sender/native/apsi/util/cuckoo_filter_table.cpp
+++ sender/native/apsi/util/cuckoo_filter_table.cpp
@@ -12,6 +12,29 @@ using namespace std;
 using namespace apsi::util;
 using namespace apsi::sender::util;
 
+namespace {
+    struct TagIndexInfo {
+        size_t tag_start_bit;
+        size_t tag_start_idx;
+        size_t tag_start_offset;
+        size_t bits_first_word;
+        size_t bits_second_word;
+
+        TagIndexInfo(size_t bits_per_tag, size_t tags_per_bucket, size_t bucket, size_t tag_idx)
+        {
+            tag_start_bit = (bucket * bits_per_tag * tags_per_bucket) + (tag_idx * bits_per_tag);
+            tag_start_idx = tag_start_bit / 64;
+            tag_start_offset = tag_start_bit % 64;
+            bits_first_word = bits_per_tag;
+            bits_second_word = 0;
+
+            if (tag_start_offset > 64 - bits_per_tag) {
+                bits_first_word = 64 - tag_start_offset;
+                bits_second_word = bits_per_tag - bits_first_word;
+            }
+        }
+    };
+}
 
 CuckooFilterTable::CuckooFilterTable(size_t num_items, size_t bits_per_tag)
     : bits_per_tag_(bits_per_tag), tag_input_mask_(static_cast<std::uint32_t>(-1) << bits_per_tag)
@@ -26,25 +49,16 @@ CuckooFilterTable::CuckooFilterTable(size_t num_items, size_t bits_per_tag)
 
 uint32_t CuckooFilterTable::read_tag(size_t bucket, size_t tag_idx) const
 {
-    size_t tag_start_bit = (bucket * bits_per_tag_ * tags_per_bucket_) + (tag_idx * bits_per_tag_);
-    size_t tag_start_idx = tag_start_bit / 64;
-    size_t tag_start_offset = tag_start_bit % 64;
-    size_t bits_first_word = bits_per_tag_;
-    size_t bits_second_word = 0;
+    TagIndexInfo tii(bits_per_tag_, tags_per_bucket_, bucket, tag_idx);
 
-    if (tag_start_offset > 64 - bits_per_tag_) {
-        bits_first_word = 64 - tag_start_offset;
-        bits_second_word = bits_per_tag_ - bits_first_word;
-    }
+    uint64_t tag_word = table_[tii.tag_start_idx];
+    uint64_t mask = ~(static_cast<uint64_t>(-1) << tii.bits_first_word);
+    uint32_t tag = static_cast<uint32_t>((tag_word >> tii.tag_start_offset) & mask);
 
-    uint64_t tag_word = table_[tag_start_idx];
-    uint64_t mask = ~(static_cast<uint64_t>(-1) << bits_first_word);
-    uint32_t tag = static_cast<uint32_t>((tag_word >> tag_start_offset) & mask);
-
-    if (bits_second_word != 0) {
-        tag_word = table_[tag_start_idx + 1];
-        mask = ~(static_cast<uint64_t>(-1) << bits_second_word);
-        tag |= (static_cast<uint32_t>(tag_word) & mask) << bits_first_word;
+    if (tii.bits_second_word != 0) {
+        tag_word = table_[tii.tag_start_idx + 1];
+        mask = ~(static_cast<uint64_t>(-1) << tii.bits_second_word);
+        tag |= (static_cast<uint32_t>(tag_word) & mask) << tii.bits_first_word;
     }
 
     return tag;
@@ -56,27 +70,18 @@ void CuckooFilterTable::write_tag(size_t bucket, size_t tag_idx, uint32_t tag)
         throw invalid_argument("tag is not constrained to bits_per_tag");
     }
 
-    size_t tag_start_bit = (bucket * bits_per_tag_ * tags_per_bucket_) + (tag_idx * bits_per_tag_);
-    size_t tag_start_idx = tag_start_bit / 64;
-    size_t tag_start_offset = tag_start_bit % 64;
-    size_t bits_first_word = bits_per_tag_;
-    size_t bits_second_word = 0;
-
-    if (tag_start_offset > 64 - bits_per_tag_) {
-        bits_first_word = 64 - tag_start_offset;
-        bits_second_word = bits_per_tag_ - bits_first_word;
-    }
+    TagIndexInfo tii(bits_per_tag_, tags_per_bucket_, bucket, tag_idx);
 
     uint64_t tag_ones = (1ull << (bits_per_tag_ + 1)) - 1;
-    uint64_t tag_mask = ~(tag_ones << tag_start_offset);
-    uint64_t tag_word = static_cast<uint64_t>(tag) << tag_start_offset;
-    table_[tag_start_idx] &= tag_mask;
-    table_[tag_start_idx] |= tag_word;
+    uint64_t tag_mask = ~(tag_ones << tii.tag_start_offset);
+    uint64_t tag_word = static_cast<uint64_t>(tag) << tii.tag_start_offset;
+    table_[tii.tag_start_idx] &= tag_mask;
+    table_[tii.tag_start_idx] |= tag_word;
 
-    if (bits_second_word != 0) {
-        tag_mask = ~(tag_ones >> bits_first_word);
-        tag_word = static_cast<uint64_t>(tag) >> bits_first_word;
-        table_[tag_start_idx + 1] &= tag_mask;
-        table_[tag_start_idx + 1] |= tag_word;
+    if (tii.bits_second_word != 0) {
+        tag_mask = ~(tag_ones >> tii.bits_first_word);
+        tag_word = static_cast<uint64_t>(tag) >> tii.bits_first_word;
+        table_[tii.tag_start_idx + 1] &= tag_mask;
+        table_[tii.tag_start_idx + 1] |= tag_word;
     }
 }
