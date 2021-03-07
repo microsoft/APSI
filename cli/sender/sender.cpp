6ee2df07e8a21cfaae4cfa422cff522370c01cc3
Kim Laine <kim.laine@microsoft.com>
2021-03-06 18:45:09 -0800

    CLI now works with arbitrarily long labels

--- cli/sender/sender.cpp
+++ cli/sender/sender.cpp
@@ -187,7 +187,7 @@ pair<shared_ptr<OPRFKey>, shared_ptr<SenderDB>> create_sender_db(
 
         try
         {
-            sender_db = make_shared<SenderDB>(psi_params, 10);
+            sender_db = make_shared<SenderDB>(psi_params, hashed_db_data[0].second.size());
             sender_db->set_data(hashed_db_data, thread_count);
             APSI_LOG_INFO("Created labeled SenderDB with " << sender_db->get_items().size() << " items");
         }
